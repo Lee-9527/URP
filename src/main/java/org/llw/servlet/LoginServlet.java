f9f5e7f5461d7776c55f1ace57f3157e0c158bd8
LLW <llwlin@sina.com>
2020-05-24 23:21:26 +0800

    test

--- src/main/java/org/llw/servlet/LoginServlet.java
+++ src/main/java/org/llw/servlet/LoginServlet.java
@@ -16,6 +16,9 @@ public class LoginServlet extends HttpServlet {
     }
 
     private void testMethod() {
-
+        int num = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(num++);
+        }
     }
 }
